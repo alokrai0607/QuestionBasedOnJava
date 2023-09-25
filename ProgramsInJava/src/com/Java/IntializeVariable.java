a7ff573f0741f842690b0bd512e0a1b013a9ec96
alokrai0607 <saelalok001@gmail.com>
2023-09-25 19:16:39 +0530

    IntializeVariable

---
+++ ProgramsInJava/src/com/Java/IntializeVariable.java
@@ -0,0 +1,15 @@
+package com.Java;
+
+public class IntializeVariable {
+
+	public static void main(String[] args) {
+		
+		int a = 0;
+		
+		for(a=0  ; a<15 ; a++) {
+			System.out.println(a);
+		}
+
+	}
+
+}

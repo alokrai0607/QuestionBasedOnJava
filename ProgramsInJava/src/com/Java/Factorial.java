d348d2bdc000ef7214b9eb906f935aef5f7ac164
alokrai0607 <saelalok001@gmail.com>
2023-09-26 20:58:03 +0530

    Factorial

---
+++ ProgramsInJava/src/com/Java/Factorial.java
@@ -0,0 +1,23 @@
+package com.Java;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter a value to calculate its factorial: ");
+
+		int n = sc.nextInt();
+
+		long value = 1; // Use long data type
+
+		for (int i = 1; i <= n; i++) {
+			value = value * i;
+		}
+		System.out.println("Factorial value of " + n + " is " + value);
+
+	}
+}

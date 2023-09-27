cb44de2bff86f4ae481857a25e9b816d514937e7
alokrai0607 <saelalok001@gmail.com>
2023-09-27 19:11:11 +0530

    Print * useing loop

--- ProgramsInJava/src/com/Java/Loop.java
+++ ProgramsInJava/src/com/Java/Loop.java
@@ -3,25 +3,21 @@ package com.Java;
 public class Loop {
 
 	public static void main(String[] args) {
-		    
-		    int even = 0;
-		    int  Odd = 0;
-		    
-			for(int i = 1 ; i <= 100 ; i++) {
-				
-				if(i%2==0) {
-			        even += i	;	
-				}else {
-					Odd += i ;
-				}
-				
+
+		int even = 0;
+		int Odd = 0;
+
+		for (int i = 1; i <= 100; i++) {
+
+			if (i % 2 == 0) {
+				even += i;
+			} else {
+				Odd += i;
 			}
-			System.out.println("Total sum of even Number is :"+even);
-			System.out.println("Total sum of Odd Number is :"+Odd);
-		
-		
-		
-		
+
+		}
+		System.out.println("Total sum of even Number is :" + even);
+		System.out.println("Total sum of Odd Number is :" + Odd);
 
 	}
 

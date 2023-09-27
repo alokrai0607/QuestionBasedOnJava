baad7491d2842935f4007260b52536bb4c13cee6
alokrai0607 <saelalok001@gmail.com>
2023-09-27 19:20:00 +0530

    WhileLoop

---
+++ ProgramsInJava/src/com/Java/WhileloopProgram.java
@@ -0,0 +1,19 @@
+package com.Java;
+
+public class WhileloopProgram {
+
+	public static void main(String[] args) {
+
+		int i = 1;
+
+		while (i <= 10) {
+
+			System.out.println(i);
+
+			i++;
+
+		}
+
+	}
+
+}

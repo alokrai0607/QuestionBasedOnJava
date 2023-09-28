2c6865ef42842ab82de1f0ae3927e528900981c5
alokrai0607 <saelalok001@gmail.com>
2023-09-28 19:46:40 +0530

    TableGenrator

---
+++ ProgramsInJava/src/com/Java/TableGenrator.java
@@ -0,0 +1,22 @@
+package com.Java;
+
+import java.util.Scanner;
+
+public class TableGenrator {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Pass any value in simple Integer you want , you will found table from 1 to 10 of your Number :");
+		
+		int n = sc.nextInt();
+		
+		for(int i=1 ; i <=10 ; i++) {
+			
+			System.out.println(n*i);
+		}
+
+	}
+
+}

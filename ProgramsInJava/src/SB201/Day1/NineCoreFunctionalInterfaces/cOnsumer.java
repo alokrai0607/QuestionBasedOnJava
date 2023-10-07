dda505ff743892d2124b0a73e311dd95aa50b4f0
alokrai0607 <saelalok001@gmail.com>
2023-10-07 21:34:57 +0530

    Consumer

--- ProgramsInJava/src/SB201/Day1/NineCoreFunctionalInterfaces/cOnsumer.java
+++ ProgramsInJava/src/SB201/Day1/NineCoreFunctionalInterfaces/cOnsumer.java
@@ -1,14 +1,25 @@
 package SB201.Day1.NineCoreFunctionalInterfaces;
 
-import java.util.function.Consumer;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.*;
 
 public class cOnsumer {
 
+//	public static void main(String[] args) {
+//		
+//		Consumer<String> str = x -> System.out.println((x.length()));
+//		
+//		str.accept("Alok Rai Shivam");
+//	}
+
 	public static void main(String[] args) {
-		
-		Consumer<String> str = x -> System.out.println((x.length()));
-		
-		str.accept("Alok Rai Shivam");
+
+		List<Integer> list = Arrays.asList(10, 20, 30, 40, 50);
+
+		Consumer<Integer> a = x -> System.out.println(x);
+
+		list.stream().forEach(a);
 	}
 
 }

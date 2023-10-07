926ce67670aa5d4a62f1ba632f6c65c0b6e8d435
alokrai0607 <saelalok001@gmail.com>
2023-10-07 21:48:10 +0530

    biConsumerForEach

--- ProgramsInJava/src/SB201/Day1/NineCoreFunctionalInterfaces/biConsumer.java
+++ ProgramsInJava/src/SB201/Day1/NineCoreFunctionalInterfaces/biConsumer.java
@@ -1,11 +1,24 @@
 package SB201.Day1.NineCoreFunctionalInterfaces;
 
-import java.util.function.BiConsumer;
+import java.util.HashMap;
+import java.util.Map;
 
 public class biConsumer {
 
+//	public static void main(String[] args) {
+//		BiConsumer<String,String> res = (a,b)->System.out.println(a+" + "+b);
+//		res.accept("Alok","Rai");
+//	}
+	
+	
 	public static void main(String[] args) {
-		BiConsumer<String,String> res = (a,b)->System.out.println(a+" + "+b);
-		res.accept("Alok","Rai");
+		Map<Integer,String> element = new HashMap<>() ;
+		element.put(95, "Alok");
+		element.put(85, "Rohit");
+		element.put(66, "Ashish");
+		element.put(90, "Shivs");
+		element.put(17, "Roshni");
+		
+		element.forEach((a,b)->System.out.println(a+" is obtain by "+b));
 	}
 }

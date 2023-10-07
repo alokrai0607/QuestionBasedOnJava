43f6adf8268a920883ce048413a26cde6e9be96f
alokrai0607 <saelalok001@gmail.com>
2023-10-07 22:17:15 +0530

    Supplier

---
+++ ProgramsInJava/src/SB201/Day1/NineCoreFunctionalInterfaces/sUpplier.java
@@ -0,0 +1,14 @@
+package SB201.Day1.NineCoreFunctionalInterfaces;
+
+import java.util.function.Supplier;
+
+public class sUpplier {
+
+	public static void main(String[] args) {
+
+		Supplier<String> k = () -> "Hello Java";
+		System.out.println(k.get());
+
+	}
+
+}

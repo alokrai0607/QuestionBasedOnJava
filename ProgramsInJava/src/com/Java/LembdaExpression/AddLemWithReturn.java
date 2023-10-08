1a0843d1ed8837cccd34a8f76c844937f0ecceef
alokrai0607 <saelalok001@gmail.com>
2023-10-08 10:54:52 +0530

    Lembda without return keyword

--- ProgramsInJava/src/com/Java/LembdaExpression/AddLemWithReturn.java
+++ ProgramsInJava/src/com/Java/LembdaExpression/AddLemWithReturn.java
@@ -8,11 +8,19 @@ interface An {
 
 public class AddLemWithReturn {
 
+//	public static void main(String[] args) {
+//
+//		An an = (a, b) -> {
+//			return (a + b);
+//		};
+//		System.out.println(an.print(25, 50));
+//
+//	}
+
 	public static void main(String[] args) {
 
-		An an = (a, b) -> {
-			return (a + b);
-		};
+		An an = (a, b) -> (a + b);
+
 		System.out.println(an.print(25, 50));
 
 	}

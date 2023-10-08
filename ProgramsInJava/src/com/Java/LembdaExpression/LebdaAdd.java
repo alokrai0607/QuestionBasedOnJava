dd6e155ec2bde4e16723bcf051ba132c8baf2853
alokrai0607 <saelalok001@gmail.com>
2023-10-08 10:40:33 +0530

    Lembda Add

---
+++ ProgramsInJava/src/com/Java/LembdaExpression/LebdaAdd.java
@@ -0,0 +1,20 @@
+package com.Java.LembdaExpression;
+
+
+interface AddTwoNumbers {
+	
+	void add(Integer x , Integer y);
+}
+
+public class LebdaAdd {
+
+	public static void main(String[] args) {
+		
+		AddTwoNumbers addTwoNumbers = (x,y) ->{
+			System.out.println(x+y);
+		};
+
+		addTwoNumbers.add(5, 5);
+	}
+
+}

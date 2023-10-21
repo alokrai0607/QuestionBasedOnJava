a2acc56358fa88ab23d49588681832001978c876
alokrai0607 <saelalok001@gmail.com>
2023-10-21 13:19:04 +0530

    DSA Sorting algos

--- ProgramsInJava/src/DSA/SortingAlgos/SelectionSortwithInteger.java
+++ ProgramsInJava/src/DSA/SortingAlgos/SelectionSortwithInteger.java
@@ -6,19 +6,17 @@ public class SelectionSortwithInteger {
 
 		int[] list = { 38, 52, 9, 18, 6, 12, 3 };
 
-		int min = 1;
-		int temp = 0;
-
 		for (int i = 0; i < list.length; i++) {
 
-			min = i;
+		int	min = i;
+		
 			for (int j = i + 1; j < list.length; j++) {
 
 				if (list[j] < list[min]) {
 					min = j;
 				}
 			}
-			temp = list[i];
+			int temp = list[i];
 			list[i] = list[min];
 			list[min] = temp;
 

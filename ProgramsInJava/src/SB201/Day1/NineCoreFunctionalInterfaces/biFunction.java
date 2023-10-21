de24980c708878fc8dc50b5dbd1a727069d2697c
alokrai0607 <saelalok001@gmail.com>
2023-10-21 14:26:22 +0530

    duplicates

--- ProgramsInJava/src/SB201/Day1/NineCoreFunctionalInterfaces/biFunction.java
+++ ProgramsInJava/src/SB201/Day1/NineCoreFunctionalInterfaces/biFunction.java
@@ -4,6 +4,7 @@ import java.util.function.BiFunction;
 
 public class biFunction {
 	public static void main(String[] args) {
+		
 		BiFunction<Integer, Integer, String> h = (a, b) -> a + "" + b;
 		System.out.println(h.apply(9, 8));
 	}
---
+++ ProgramsInJava/src/zquestions/Interview/FindDuplicates.java
@@ -0,0 +1,30 @@
+package zquestions.Interview;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class FindDuplicates {
+
+	public static void main(String[] args) {
+		//int[] arr = { 1, 2, 3, 4, 2, 5, 6, 3, 7, 8, 1 };
+		int[] arr = {};
+		ArrayList<Integer> duplicates = new ArrayList<>();
+		HashSet<Integer> set = new HashSet<>();
+
+		for (int num : arr) {
+			if (!set.add(num) && !duplicates.contains(num)) {
+	
+					duplicates.add(num);
+			}
+		}
+
+		if (duplicates.isEmpty()) {
+			System.out.println(-1);
+		} else {
+			System.out.println("Duplicates present in the array are: ");
+			for (int num : duplicates) {
+				System.out.print(num + " ");
+			}
+		}
+	}
+}

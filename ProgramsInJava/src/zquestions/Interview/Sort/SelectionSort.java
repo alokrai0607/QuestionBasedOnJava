8874938f6155fe573fab95f21aebc1982f440524
alokrai0607 <saelalok001@gmail.com>
2023-10-21 13:19:56 +0530

    Hello duniya

---
+++ ProgramsInJava/src/zquestions/Interview/Sort/SelectionSort.java
@@ -0,0 +1,29 @@
+package zquestions.Interview.Sort;
+
+public class SelectionSort {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 21, 54, 28, 56, 97, 52, 88, 11, 0 };
+
+		for (int i = 0; i < arr.length; i++) {
+			int min = i;
+			for (int j = i + 1; j < arr.length; j++) {
+
+				if (arr[j] > arr[min]) {
+					min = j;
+				}
+			}
+
+			int tem = arr[i];
+			arr[i] = arr[min];
+			arr[min] = tem;
+
+		}
+		for (int a : arr) {
+			System.out.print(a + " ");
+		}
+
+	}
+
+}

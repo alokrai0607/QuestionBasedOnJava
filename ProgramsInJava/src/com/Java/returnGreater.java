6e7f6504db992b0df7515355f5002dc90224c5e1
alokrai0607 <saelalok001@gmail.com>
2023-10-22 18:48:07 +0530

    scn

--- ProgramsInJava/src/com/Java/returnGreater.java
+++ ProgramsInJava/src/com/Java/returnGreater.java
@@ -5,27 +5,28 @@ import java.util.Scanner;
 public class returnGreater {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Please pass any 3 Numbrs in your console you will find your Grater and Smaller both value ");
-		
+		Scanner scn = new Scanner(System.in);
+
+		System.out.println(
+				"Please pass any 3 Numbrs in your console " + 
+		        "you will find your Grater and Smaller both value ");
+
 		System.out.println("Please pass the value of a :");
-		int a = sc.nextInt();
-		
+		int a = scn.nextInt();
+
 		System.out.println("Please pass the value of b :");
-		int b = sc.nextInt();
-		
+		int b = scn.nextInt();
+
 		System.out.println("Please pass the value of c :");
-		int c = sc.nextInt();
-		
-		
-		if(a>b && a>c) {
-			System.out.println(a+" is grater than "+b+" and "+c);
-		}else if (b>a && b>c) {
-			System.out.println(b+" is grater than "+a+" and "+c);
-		}else if (c>a && c>b) {
-			System.out.println(c+" is grater than "+a+" and "+b);
-		}else if (a==b && a==c && b==c) {
+		int c = scn.nextInt();
+
+		if (a > b && a > c) {
+			System.out.println(a + " is grater than " + b + " and " + c);
+		} else if (b > a && b > c) {
+			System.out.println(b + " is grater than " + a + " and " + c);
+		} else if (c > a && c > b) {
+			System.out.println(c + " is grater than " + a + " and " + b);
+		} else if (a == b && a == c && b == c) {
 			System.out.println("all are eqal ");
 		}
 	}

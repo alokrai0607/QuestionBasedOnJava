69f278a64052eb1b0e3a34df1c48d92926d9ee7c
alokrai0607 <saelalok001@gmail.com>
2023-10-22 18:45:42 +0530

    grater program

---
+++ ProgramsInJava/src/com/Java/returnGreater.java
@@ -0,0 +1,32 @@
+package com.Java;
+
+import java.util.Scanner;
+
+public class returnGreater {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Please pass any 3 Numbrs in your console you will find your Grater and Smaller both value ");
+		
+		System.out.println("Please pass the value of a :");
+		int a = sc.nextInt();
+		
+		System.out.println("Please pass the value of b :");
+		int b = sc.nextInt();
+		
+		System.out.println("Please pass the value of c :");
+		int c = sc.nextInt();
+		
+		
+		if(a>b && a>c) {
+			System.out.println(a+" is grater than "+b+" and "+c);
+		}else if (b>a && b>c) {
+			System.out.println(b+" is grater than "+a+" and "+c);
+		}else if (c>a && c>b) {
+			System.out.println(c+" is grater than "+a+" and "+b);
+		}else if (a==b && a==c && b==c) {
+			System.out.println("all are eqal ");
+		}
+	}
+}

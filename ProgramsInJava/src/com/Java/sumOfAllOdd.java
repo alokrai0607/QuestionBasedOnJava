97d7d61a7d5ffb93e3cb58fa1a1896973ad53b88
alokrai0607 <saelalok001@gmail.com>
2023-10-22 18:34:19 +0530

    sumOfAllOdd

--- ProgramsInJava/src/com/Java/sumOfAllOdd.java
+++ ProgramsInJava/src/com/Java/sumOfAllOdd.java
@@ -1,10 +1,29 @@
 package com.Java;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class sumOfAllOdd {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
 
-	}
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Please enter the length of the list:");
+        int n = sc.nextInt();
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            list.add(sc.nextInt());
+        }
+        int sum = 0;
+        for (int i : list) {
+            if (i % 2 == 1) { 
+                sum += i; 
+            }
+        }
+        System.out.println("Output will be: " + sum);
+    }
 }

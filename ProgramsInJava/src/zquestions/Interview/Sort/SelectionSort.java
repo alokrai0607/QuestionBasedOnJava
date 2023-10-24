cb9244c20babf47d35ed2711b9f2b42c7f25d55c
alokrai0607 <saelalok001@gmail.com>
2023-10-24 10:56:55 +0530

    selection sort

--- ProgramsInJava/src/zquestions/Interview/Sort/SelectionSort.java
+++ ProgramsInJava/src/zquestions/Interview/Sort/SelectionSort.java
@@ -1,33 +1,30 @@
 package zquestions.Interview.Sort;
 
+
+
 public class SelectionSort {
 
 	public static void main(String[] args) {
 
 		int[] arr = { 21, 54, 28, 56, 97, 52, 88, 11, 0 };
 
-		for (int i = 0; i < arr.length; i++) {
+		for(int i=0 ; i<arr.length ; i++) {
 			
-			int min = i;
+			int min = i ; 
 			
-			for (int j = i + 1; j < arr.length; j++) {
-
-				if (arr[j] > arr[min]) {
-			
+			for(int j=i+1 ; j<arr.length ; j++) {
+				if(arr[j]>arr[min]) {
 					min = j;
 				}
 				
 			}
-
-			int tem = arr[i];
+			int t = arr[i];
 			arr[i] = arr[min];
-			arr[min] = tem;
-
+			arr[min]=t;
 		}
-		for (int a : arr) {
-			System.out.print(a + " ");
+		for(int s : arr) {
+			System.out.println(s+" ");
 		}
-
 	}
 
 }

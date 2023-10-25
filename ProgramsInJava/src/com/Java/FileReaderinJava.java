f0e9af5b3fa4b0b92ee246bba8156f3c4c9c5e2c
alokrai0607 <saelalok001@gmail.com>
2023-10-25 13:10:22 +0530

    FileReaderinJava

---
+++ ProgramsInJava/src/com/Java/FileReaderinJava.java
@@ -0,0 +1,17 @@
+package com.Java;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderinJava {
+
+	public static void main(String[] args) throws IOException {
+		FileReader fr=new FileReader("d://abc.txt");
+		int i=fr.read();
+		while( i != -1) {
+		System.out.print((char) i);
+		i = fr.read();
+		}
+		}
+
+}

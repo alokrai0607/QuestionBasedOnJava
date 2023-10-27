ab45e410e9378b7a0688c7de6f65e89df13ac3bd
alokrai0607 <saelalok001@gmail.com>
2023-10-27 18:36:08 +0530

    Bubble

--- ProgramsInJava/src/zquestions/Interview/Sort/BubbleSort.java
+++ ProgramsInJava/src/zquestions/Interview/Sort/BubbleSort.java
@@ -4,27 +4,26 @@ public class BubbleSort {
 
 	public static void main(String[] args) {
 
-		int[] arr = { 21, 56, 25, 85, 75, 95, 58 };
+		int[] arr = { 21, 56, 25, 85, 75, 95, 58, 21, 56, 25, 85, 75, 95, 5821, 56, 25, 85, 75, 95, 58 };
 
-		for (int i = 0; i < arr.length; i++) {
-			boolean flag = false;
-			for (int j = 0; j < arr.length - 1 - i; j++) {
-
-				if (arr[j] < arr[j + 1]) {
-					int temp = arr[j];
-					arr[j] = arr[j + 1];
-					arr[j + 1] = temp;
-					flag = false;
+		for(int i=0 ; i<arr.length ; i++) {
+			
+			boolean f = false ; 
+			for(int  j=0 ; j<arr.length-1-i;j++) {
+				
+				if(arr[j]>arr[j+1]) {
+					int tem = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = tem;
+					f=true;
 				}
-
 			}
-			if (flag = false) {
+			if(!f) {
 				break;
 			}
 		}
-
-		for (int a : arr) {
-			System.out.println(a + " ");
+		for(int k : arr) {
+			System.out.println(k+" ");
 		}
 	}
 
--- ProgramsInJava/src/zquestions/Interview/Sort/selectionsortwitString.java
+++ ProgramsInJava/src/zquestions/Interview/Sort/selectionsortwitString.java
@@ -3,26 +3,25 @@ package zquestions.Interview.Sort;
 public class selectionsortwitString {
 
 	public static void main(String[] args) {
-		String[] a = {"manyata" , "dravyansh" , "ayush" , "lucky" , "choti", "ritika"};
-		
-		for(int i = 0 ;i < a.length ; i++) {
-			int min = i ; 
-			for(int j=i+1 ; j<a.length ; j++) {
-				
-				if(a[j].compareToIgnoreCase(a[min])>0) {
-					
-					min = j ; 
+		String[] a = { "manyata", "dravyansh", "ayush", "lucky", "choti", "ritika" };
+
+		for (int i = 0; i < a.length; i++) {
+			int min = i;
+			for (int j = i + 1; j < a.length; j++) {
+
+				if (a[j].compareToIgnoreCase(a[min]) > 0) {
+
+					min = j;
 				}
 			}
 			String temp = a[i];
 			a[i] = a[min];
 			a[min] = temp;
-			
-			
+
 		}
-		  for(String you : a) {
-			  System.out.println(you+" ");
-		  }
+		for (String you : a) {
+			System.out.println(you + " ");
+		}
 
 	}
 

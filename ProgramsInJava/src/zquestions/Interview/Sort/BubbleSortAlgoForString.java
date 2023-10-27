3e80fdc94f7cb39e7dcbbc0bd24c87697d388c3e
alokrai0607 <saelalok001@gmail.com>
2023-10-27 18:42:42 +0530

    Bubsitring

--- ProgramsInJava/src/zquestions/Interview/Sort/BubbleSortAlgoForString.java
+++ ProgramsInJava/src/zquestions/Interview/Sort/BubbleSortAlgoForString.java
@@ -6,23 +6,25 @@ public class BubbleSortAlgoForString {
 
         String[] a = {"ziya", "roshni", "alok", "Tanay", "ashish"};
 
-        for (int i = 0; i < a.length; i++) {
-            boolean flag = false;
-            for (int j = 0; j < a.length - 1 - i; j++) {
-
-                if (a[j].compareToIgnoreCase(a[j + 1]) > 0) { // Use compareToIgnoreCase
-                    String temp = a[j];
-                    a[j] = a[j + 1];
-                    a[j + 1] = temp;
-                    flag = true;
-                }
-            }
-            if (!flag) {
-                break;
-            }
-        }
-        for (String i : a) {
-            System.out.println(i + " ");
-        }
+         for(int i=0 ; i<a.length ; i++) {
+        	 int f = 0 ;
+        	 for(int j=0 ; j<a.length-1-i ; j++) {
+        		 
+        		 if(a[j].compareToIgnoreCase(a[j+1])>0) {
+        			 String t = a[j];
+        			 a[j] = a[j+1];
+        			 a[j+1] = t ;
+        			 f = 1;
+        		 }
+        	 }
+        	 if(f==0) {
+        		 break;
+        	 }
+         }
+         for(String k : a) {
+        	 System.out.println(k+" ");
+         }
+        
+        
     }
 }

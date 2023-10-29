264910d8b88e2303ae511d98fdf97d7b05460be7
alokrai0607 <saelalok001@gmail.com>
2023-10-29 12:43:16 +0530

    asdf

--- My1stJDBCProject/src/main/java/com/masai/my1JDBC.java
+++ My1stJDBCProject/src/main/java/com/masai/my1JDBC.java
@@ -8,17 +8,25 @@ import java.sql.Statement;
 public class my1JDBC {
 
 	public static void main(String[] args) throws ClassNotFoundException {
+		//Register the driver class
 		Class.forName("com.mysql.cj.jdbc.Driver");
+		//set path in variable
 		String url = "jdbc:mysql://localhost:3306/jdbcdb";
 		try {
+		//	Create the connection object
 			Connection con = DriverManager.getConnection(url, "root", "0303");
+			
+			//Create the Statement object
 			Statement st = con.createStatement();
+			
+			//Execute the query
 			int row = st.executeUpdate("insert into student values(3,'alok','varanasi','1254425100')");
 			if (row > 0) {
 				System.out.println("Student detail has inserted..");
 			} else {
 				System.out.println("Student detail has not inserted..");
 			}
+			//Close the connection object
 			con.close();
 		} catch (SQLException e) {
 			e.printStackTrace();

ac75fa2cd446903bbe88595370852d7205d946ba
alokrai0607 <saelalok001@gmail.com>
2023-11-06 22:21:44 +0530

    BalancedParenthisis

---
+++ HUKUMU_PORTAL/src/Interview/Range5_6/DSA/BalancedParenthesesExample1.java
@@ -0,0 +1,66 @@
+package Interview.Range5_6.DSA;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class BalancedParenthesesExample1 {
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static boolean balancedParenthesis(String inputStr) {
+
+		Stack stack = new Stack();
+
+		char[] charArray = inputStr.toCharArray();
+
+		for (int i = 0; i < charArray.length; i++) {
+
+			char current = charArray[i];
+
+			if (current == '{' || current == '[' || current == '(') {
+
+				stack.push(current);
+				continue;
+			}
+
+			if (stack.isEmpty()) {
+				return false;
+			}
+
+			char popChar;
+			switch (current) {
+			case ')':
+				popChar = (char) stack.pop();
+				if (popChar == '{' || popChar == '[')
+					return false;
+				break;
+			case '}':
+				popChar = (char) stack.pop();
+				if (popChar == '(' || popChar == '[')
+					return false;
+				break;
+			case ']':
+				popChar = (char) stack.pop();
+				if (popChar == '(' || popChar == '{')
+					return false;
+				break;
+			}
+		}
+		return (stack.isEmpty());
+	}
+
+	public static void main(String[] args) {
+
+		String inputStr;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter input string to check:");
+
+		inputStr = sc.nextLine();
+
+		if (balancedParenthesis(inputStr))
+			System.out.println("Input string " + inputStr + " is balanced.");
+		else
+			System.out.println("Input string " + inputStr + " is not balanced.");
+	}
+}

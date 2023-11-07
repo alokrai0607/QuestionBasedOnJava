e2f65c323e3cde877798c9b08102b2262928a79f
alokrai0607 <saelalok001@gmail.com>
2023-11-07 20:11:57 +0530

    Stringwala

--- ProgramsInJava/src/zquestions/Interview/Sort/BubbleSortAlgoForString.java
+++ ProgramsInJava/src/zquestions/Interview/Sort/BubbleSortAlgoForString.java
@@ -3,28 +3,25 @@ package zquestions.Interview.Sort;
 public class BubbleSortAlgoForString {
 
     public static void main(String[] args) {
-
-        String[] a = {"ziya", "roshni", "alok", "Tanay", "ashish"};
-
-         for(int i=0 ; i<a.length ; i++) {
-        	 int f = 0 ;
-        	 for(int j=0 ; j<a.length-1-i ; j++) {
-        		 
-        		 if(a[j].compareToIgnoreCase(a[j+1])>0) {
-        			 String t = a[j];
-        			 a[j] = a[j+1];
-        			 a[j+1] = t ;
-        			 f = 1;
-        		 }
-        	 }
-        	 if(f==0) {
-        		 break;
-        	 }
-         }
-         for(String k : a) {
-        	 System.out.println(k+" ");
-         }
-        
+    String[] pot = {"roshni", "ravina" , "alok" , "shivam" , "vivek"};
+    
+    for(int i = 0 ; i< pot.length ; i++) {
+    	boolean data = false ; 
+    	for(int j= 0 ; j<pot.length-1-i ; j++) {
+    		if(pot[j].compareToIgnoreCase(pot[j+1])<0) {
+    			String tem = pot[j];
+    			pot[j] = pot[j+1];
+    			pot[j+1] = tem;
+    			data = true;
+    		}
+    	}
+    	if(!data) {
+    		break;
+    	}
+    }
+        for(String dada : pot) {
+        	System.out.println(dada+" ");
+        }
         
     }
 }

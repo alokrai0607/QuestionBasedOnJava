ca67feaa2f33fc9e6db25d6b7219ad477e4ffce4
alokrai0607 <saelalok001@gmail.com>
2023-11-09 14:39:39 +0530

    Febbonachi

---
+++ ProgramsInJava/src/zquestions/Interview/ClimbStairesFebinocci.java
@@ -0,0 +1,23 @@
+package zquestions.Interview;
+
+public class ClimbStairesFebinocci {
+
+	public static void main(String[] args) {
+		
+		System.out.println(climbStairs(5));
+
+	}
+	
+	 public static int climbStairs(int n) {
+	        if(n<=3){
+	          return n;
+	        }
+	        int a=3 , b=2;
+	        for(int i=0 ; i<n-3 ; i++){
+	            a = a+b;
+	            b = a-b;
+	        }
+	        return a;
+	    }
+
+}

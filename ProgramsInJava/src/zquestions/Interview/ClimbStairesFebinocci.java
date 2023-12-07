9013784ae699eda21e034890acce20a334efa215
alokrai0607 <saelalok001@gmail.com>
2023-12-07 21:49:52 +0530

    ProgramsJava

--- ProgramsInJava/src/zquestions/Interview/ClimbStairesFebinocci.java
+++ ProgramsInJava/src/zquestions/Interview/ClimbStairesFebinocci.java
@@ -13,7 +13,9 @@ public class ClimbStairesFebinocci {
 	          return n;
 	        }
 	        int a=3 , b=2;
+	        
 	        for(int i=0 ; i<n-3 ; i++){
+	        	
 	            a = a+b;
 	            b = a-b;
 	        }

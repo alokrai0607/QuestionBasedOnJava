551e22f7e9c3ed6187f94ab692607512d7619574
alokrai0607 <saelalok001@gmail.com>
2023-12-17 16:11:58 +0530

    Hello Bubble

--- ProgramsInJava/src/zquestions/Interview/Sort/BubbleSort.java
+++ ProgramsInJava/src/zquestions/Interview/Sort/BubbleSort.java
@@ -2,20 +2,21 @@ package zquestions.Interview.Sort;
 
 public class BubbleSort {
     public static void main(String args[]) {
-        int arr[] = {21, 25, 85, 95, 75, 85, 65};
-
-        for (int i = arr.length - 1; i >= 0; i--) {
-            for (int j = 0; j <= i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
-        for (int k : arr) {
-            System.out.print(k + " ");
-        }
+      int[] arr = {1,8 , 7 , 6 , 25 , 85 ,0 , 25};
+      
+      for(int i=arr.length-1 ; i>=0 ; i--) {
+    	  for(int j=0 ; j<=i-1 ; j++) {
+    		  if(arr[j]<arr[j+1]) {
+    			  int temp = arr[j];
+    			  arr[j] = arr[j+1];
+    			  arr[j+1] = temp;
+    		  }
+    	  }
+    	  }
+    	  for (int l : arr) {
+    		  System.out.println(l+" ");
+    	  }
+      }
     }
-}
+
 

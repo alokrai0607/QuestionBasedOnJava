fb64bb09d1078a5ecdbbec66b43eb1494f39c30b
alokrai0607 <saelalok001@gmail.com>
2024-01-29 17:30:03 +0530

    pattern

--- ProgramsInJava/src/patternPrinting/Main.java
+++ ProgramsInJava/src/patternPrinting/Main.java
@@ -1,16 +1,27 @@
 package patternPrinting;
 
+/*
+
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+
+*/
+
+
+
 public class Main {
     public static void main(String[] args) {
-        int rows = 5; // Number of rows in the pattern
-
+        int rows = 5; 
   
         for (int i = rows; i > 0; i--) {
             
             for (int j = 0; j < i; j++) {
            
-            	System.out.print("* ");
             
+            	System.out.print("* ");
             }
            
             System.out.println();
---
+++ ProgramsInJava/src/patternPrinting/newpattern3.java
@@ -0,0 +1,39 @@
+package patternPrinting;
+
+/*
+
+* * * * * * * * * * 
+* * * * * * * * * 
+* * * * * * * * 
+* * * * * * * 
+* * * * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+
+*/
+
+
+
+public class newpattern3 {
+
+	public static void main(String[] args) {
+		
+		int row = 10 ; 
+		
+		for(int i = 0 ; i<row ; i++) {
+			
+			for(int j=i ; j<row ; j++) {
+				
+				
+				System.out.print("* ");
+				
+			}
+			System.out.println();
+		}
+
+	}
+
+}
